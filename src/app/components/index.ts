3b14829b658181658f648783b5b520713e80e608
dasha1245 <dcherkasova75@gmail.com>
2022-10-25 12:10:33 +0300

    lesson1

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,2 +1,8 @@
 export * from './users/users.component'
 export * from './user/user.component'
+
+export * from './posts/posts.component'
+export * from './post/post.component'
+
+export * from './comments/comments.component'
+export * from './comment/comment.component'
--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,1 +1,5 @@
 export * from './user.service'
+
+export * from './post.service'
+
+export * from './comment.service'
